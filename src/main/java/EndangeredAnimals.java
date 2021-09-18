b701c22c9e7ac727cec451da9e09034c9d05cbd0
Your Name <youremail@domain.com>
2021-09-18 20:09:39 +0300

    added the unique values to the endangered animals

--- src/main/java/EndangeredAnimals.java
+++ src/main/java/EndangeredAnimals.java
@@ -1,6 +1,10 @@
 public class EndangeredAnimals extends Wildlife {
+    private String health;
+    private String age;
     public EndangeredAnimals(int id,String name){
         this.id = id;
         this.name = name;
+        this.health = health;
+        this.age = age;
     }
 }
